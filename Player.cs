0f7f934a9e1687c8556774e2e7c5a0dce1fde5a4
em-twines <emdelbridge@gmail.com>
2023-03-24 16:02:26 -0400

    edited ship constructor + fixed rnd.Next in PlaceShip decidingInt

--- Player.cs
+++ Player.cs
@@ -28,46 +28,50 @@ namespace CSharpBattleShip
 
         }
 
-        private int[,] CheckMatrices()
-        {
+    //    private int[,] CheckMatrices()
+    //    {
 
-            int[,] arr3 = new int[21, 21];
-            int[,] arr6 = new int[21, 21];
-            int[,] arr7 = new int[21, 21];
+    //        int total = 0;
+    //        int[,] arr3 = new int[21, 21];
+    //        int[,] arr6 = new int[21, 21];
+    //        int[,] arr7 = new int[21, 21];
 
-            int i, j, n;
-            n = 3;
-
-
-            while (np.count_nonzero(board_e == 4) != 2) or(np.count_nonzero(board_e == 2)) != 16:
+    //        int i, j, n;
+    //        n = 3;
 
 
-            for (i = 0; i < n; i++)
-                for (j = 0; j < n; j++)
-                    arr3[i, j] = aircraftCarrier.newBoard[i, j] + battleship.newBoard[i, j];
+    //        while (total != 16)
+    //        {
 
-            for (i = 0; i < n; i++)
-                for (j = 0; j < n; j++)
-                    arr6[i, j] = submarine.newBoard[i, j] + destroyer.newBoard[i, j];
+    //            for (i = 0; i < n; i++)
+    //                for (j = 0; j < n; j++)
+    //                    arr3[i, j] = aircraftCarrier.newBoard[i, j] + battleship.newBoard[i, j];
 
-            for (i = 0; i < n; i++)
-                for (j = 0; j < n; j++)
-                    arr7[i, j] = arr3[i, j] + arr6[i, j];
+    //            for (i = 0; i < n; i++)
+    //                for (j = 0; j < n; j++)
+    //                    arr6[i, j] = submarine.newBoard[i, j] + destroyer.newBoard[i, j];
 
+    //            for (i = 0; i < n; i++)
+    //                for (j = 0; j < n; j++)
+    //                    arr7[i, j] = arr3[i, j] + arr6[i, j];
 
-            List<int> list=new List<int>(21*21);
-
-
-            for (int e = 0; e < 21; e++)
-            {
-                for (int f = 0; f < 21; f++)
-                    list.Add(arr7[e, f]);
-            }
-
-        }
+    //            List<int> flatArray = FlattenArray(arr7);
+    //            total = flatArray.Sum();
+    //        }
+    //    }
 
 
+    //    List<int> FlattenArray(int[,] arr)
+    //    {
+    //        List<int> list = new List<int>(21 * 21);
 
+    //        for (int e = 0; e < 21; e++)
+    //        {
+    //            for (int f = 0; f < 21; f++)
+    //                list.Add(arr[e, f]);
+    //        }
+    //        return list;
+    //    }
 
-    }
 }
+    }
--- Ship.cs
+++ Ship.cs
@@ -2,18 +2,20 @@ namespace CSharpBattleShip
 {
     public class Ship
     {
-        private string name = "";
-        private int size = 0;
-        private int health = 0;
+        private string name;
+        protected int size;
+        private int health;
         // private int[] location = new int[] { };
         public int[,] newBoard = new int[21,21];
 
         public Ship(string name, int size, int health)
         {
-
+            this.name = name;
+            this.size = size;
+            this.health = health;
         }
 
-        private void PlaceShipsX()
+        public int[,] PlaceShipsX()
         {
             Random rnd = new Random();
             int starting_position_x = rnd.Next(1, (21 - this.size + 1));
@@ -32,24 +34,24 @@ namespace CSharpBattleShip
                     {
                         if (y == starting_position_y & x == starting_position_x)
                         {
-                            this.newBoard[y, x] = 2;
+                            //this.newBoard[y, x] = 2;
+                            newBoard[y, x] = 2;
                             starting_position_x += 1;
                         }
                     }
                 }
 
             }
-
-            Console.WriteLine(this.newBoard);
+            return newBoard;
 
         }
-        private void PlaceShipsY()
+        public int[,] PlaceShipsY()
         {
             Random rnd = new Random();
             int starting_position_y = rnd.Next(1, (21 - this.size + 1));
             int starting_position_x = rnd.Next(1, 21);
 
-
+            
             int y_span_until = starting_position_y + this.size;
 
 
@@ -62,22 +64,24 @@ namespace CSharpBattleShip
                     {
                         if (y == starting_position_y & x == starting_position_x)
                         {
-                            this.newBoard[y, x] = 2;
+                            //this.newBoard[y, x] = 2;
+                            newBoard[y, x] = 2;
+
                             starting_position_x += 1;
                         }
                     }
                 }
 
             }
+            return newBoard;
 
-            Console.WriteLine(this.newBoard);
 
         }
 
         public void PlaceShips()
         {
             Random rnd = new Random();
-            int decidingInt = rnd.Next(0,1);
+            int decidingInt = rnd.Next(0,2);
             if (decidingInt == 0)
             {
                 PlaceShipsX();
@@ -86,7 +90,6 @@ namespace CSharpBattleShip
             {
                 PlaceShipsY(); 
             }
-            Console.WriteLine(newBoard);
             
         }
     
